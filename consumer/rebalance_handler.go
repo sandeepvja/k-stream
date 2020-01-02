7cfb168c3a57bf76bb70a73344104489190fa2c0
gmbyapa <gmbyapa@gmail.com>
2020-01-02 10:41:16 +0530

    consumer partition wait removed

--- consumer/rebalance_handler.go
+++
@@ -1,8 +0,0 @@
-package consumer
-
-import "context"
-
-type ReBalanceHandler interface {
-	OnPartitionRevoked(ctx context.Context, revoked []TopicPartition) error
-	OnPartitionAssigned(ctx context.Context, assigned []TopicPartition) error
-}
